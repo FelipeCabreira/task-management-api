79516b338f3fbb1b4a7c37d826abbf8839a02ead
FelipeCabreira <felipe.cabreira@edu.pucrs.br>
2023-09-03 10:58:45 -0300

    feat: config prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   semi: true, // Use semicolons at the end of statements
   singleQuote: true, // Use single quotes for strings
-  trailingComma: 'es5', // Add trailing commas wherever applicable (es5 style)
-  printWidth: 80, // Wrap lines when they exceed 80 characters
+  trailingComma: 'all', // Add trailing commas wherever applicable (es5 style)
+  printWidth: 100, // Wrap lines when they exceed 80 characters
   tabWidth: 2, // Use 2 spaces for indentation
   useTabs: false, // Use spaces for indentation
   jsxSingleQuote: false, // Use double quotes for JSX attributes
