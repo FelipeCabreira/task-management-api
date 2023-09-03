fbb108b5bcae0dc36ed5b1ba2dca689f0bf34b86
FelipeCabreira <felipe.cabreira@edu.pucrs.br>
2023-09-03 11:27:14 -0300

    fix: chalk version issue with TS < v5

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import apiRouter from './routes/api';
+import chalk from 'chalk';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,6 +12,10 @@ app.use(cors());
 
 app.use('/api', apiRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+try {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+} catch (error: any) {
+  console.error(chalk.red(`Error on index: ${error.message}`));
+}
