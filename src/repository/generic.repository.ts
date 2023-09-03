90910797217c1c6a4c8909aa19a29768e3124a1f
FelipeCabreira <felipe.cabreira@edu.pucrs.br>
2023-09-03 17:49:13 -0300

    feat: add generic repo class

---
+++ src/repository/generic.repository.ts
@@ -0,0 +1,32 @@
+import { Document, Model, Types } from 'mongoose';
+import { InvalidMongooseIdError } from 'src/exceptions/InvalidMongooseIdError';
+
+export abstract class GenericRepository<T, D extends Document, F> {
+  public model: Model<T>;
+  public fields: F[];
+
+  protected validateId(id: string) {
+    if (!Types.ObjectId.isValid(id)) {
+      throw new InvalidMongooseIdError('Provided invalid id');
+    }
+  }
+
+  async save(doc: D): Promise<D> {
+    return await doc.save();
+  }
+
+  async getById(id: string): Promise<D | null> {
+    this.validateId(id);
+    return await this.model.findById(id, this.fields.join(' '));
+  }
+
+  async create(data: T): Promise<D> {
+    const newInstance = new this.model(data);
+    return await (newInstance as D).save();
+  }
+
+  async delete(id: string): Promise<void> {
+    this.validateId(id);
+    await this.model.findByIdAndDelete(id);
+  }
+}
