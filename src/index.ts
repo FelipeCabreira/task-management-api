0e65979d68d296b557ba8bea39543b3a3b630913
FelipeCabreira <felipe.cabreira@edu.pucrs.br>
2023-09-03 11:39:54 -0300

    feat: add chalk with log messages for server

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,26 @@
-import express from 'express';
+import express, { Application } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import apiRouter from './routes/api';
 import chalk from 'chalk';
+import { logger } from './config/logger';
 
-const app = express();
+const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(cors());
+app.use(logger);
 
 app.use('/api', apiRouter);
 
 try {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  app.listen(PORT, (): void => {
+    const server = `http://localhost:${PORT}`;
+    console.log(chalk.green(`Connected successfully - server is running on ${chalk.bold(server)}`));
+    console.log(chalk.cyan(`API is running on ${chalk.bold(`${server}/`)}`));
   });
 } catch (error: any) {
   console.error(chalk.red(`Error on index: ${error.message}`));
