4b22c82f7ceaa733663ae20d178f893027262d97
FelipeCabreira <felipe.cabreira@edu.pucrs.br>
2023-09-03 23:39:25 -0300

    feat: add controller board

---
+++ src/controllers/board.controller.ts
@@ -0,0 +1,28 @@
+import { BoardService } from '@services';
+import Container from 'typedi';
+import {
+  Param,
+  Body,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Controller,
+  QueryParams,
+  UseBefore,
+  CurrentUser,
+  Authorized,
+} from 'routing-controllers';
+import { CreateBoardPayload } from '@types/request';
+
+@Controller('/board')
+export class BoardController {
+  boardService: BoardService;
+
+  constructor() {
+    this.boardService = Container.get(BoardService);
+  }
+
+  @Post('/')
+  createBoard(@Body() board: CreateBoardPayload) {}
+}
