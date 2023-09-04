24691ca240382b6e831e37721c220723e9e4f1fd
FelipeCabreira <felipe.cabreira@edu.pucrs.br>
2023-09-03 23:38:41 -0300

    feat: board services

---
+++ src/services/board.service.ts
@@ -0,0 +1,22 @@
+import { BoardRepository } from '@repository';
+import { Inject, Service } from 'typedi';
+import { NotFoundError } from 'routing-controllers';
+import { BoardMapper } from '@mappers';
+import { BoardDTO } from '@types-dto';
+
+@Service()
+export class BoardService {
+  boardRepository: BoardRepository;
+
+  constructor(@Inject() boardRepository: BoardRepository) {
+    this.boardRepository = boardRepository;
+  }
+
+  async getBoard(boardId: string): Promise<BoardDTO | null> {
+    const board = await this.boardRepository.getById(boardId);
+    if (!board) {
+      throw new NotFoundError('Board was not found');
+    }
+    return BoardMapper(board);
+  }
+}
