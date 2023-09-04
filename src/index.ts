81815f92f5686d93ef232ff9990b13717fa67d29
FelipeCabreira <felipe.cabreira@edu.pucrs.br>
2023-09-03 23:39:37 -0300

    feat: update main.ts

--- src/index.ts
+++ src/index.ts
@@ -4,24 +4,38 @@ import cors from 'cors';
 import apiRouter from './routes/api';
 import chalk from 'chalk';
 import { databaseConnect, logger } from '@config';
+import { environment } from '@config';
+import { useExpressServer } from 'routing-controllers';
+import { controllers } from '@controllers';
 
 const app: Application = express();
-const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 app.use(logger);
 
-app.use('/api', apiRouter);
+// app.use('/api', apiRouter);
+
+const routingControllersOptions = {
+  cors: true,
+  routePrefix: environment.app.routePrefix,
+  classTransformer: false,
+  defaultErrorHandler: false,
+  controllers,
+};
+
+useExpressServer(app, routingControllersOptions);
 
 databaseConnect();
 
 try {
-  app.listen(PORT, (): void => {
-    const server = `http://localhost:${PORT}`;
+  app.listen(environment.app.port, (): void => {
+    const server = `http://localhost:${environment.app.port}`;
     console.log(chalk.green(`Connected successfully - server is running on ${chalk.bold(server)}`));
-    console.log(chalk.cyan(`API is running on ${chalk.bold(`${server}/`)}`));
+    console.log(
+      chalk.cyan(`API is running on ${chalk.bold(`${server}/${environment.app.routePrefix}`)}`),
+    );
   });
 } catch (error: any) {
   console.error(chalk.red(`Error on index: ${error.message}`));
